a7f7c32de2bb32dd74e8be639f2000c037979fac
Neeraj-1007 <neerajkumarjha10@gmail.com>
2023-01-22 16:36:27 +0530

    First Commit

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+    private static  Logger logger=Logger.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.debug("Hi its me in maven");
+    }
+}
